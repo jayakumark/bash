92c0ccb4ee3bf0f62ca5b87720dd6caeea0e4a03
Adam Patterson <adamthepatterson@gmail.com>
2014-10-08 21:45:12 -0700

    .

--- digitalocean-newdroplet.sh
+++ digitalocean-newdroplet.sh
@@ -6,11 +6,7 @@ API_ENDPOINT="https://api.digitalocean.com/v2/droplets"
 
 JSON='{"name":"tempdroplet","region":"sfo1","size":"512mb","image":6375382}'
 
-echo $JSON
-
 # Code
 echo "Creating a droplet on digitalocean..."
-curl -X POST "$API_ENDPOINT" \
-	-d$JSON \
-	-H "Authorization: Bearer $TOKEN" \
-	-H "Content-Type: application/json"
+
+curl -qSs -X POST "$API_ENDPOINT" -d$JSON -H "Authorization: Bearer $TOKEN" -H "Content-Type: application/json"
