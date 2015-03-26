ae1477b4f848c1c5ca599adcaad6a5c318d217a9
Adam Patterson <adamthepatterson@gmail.com>
2015-03-25 21:04:12 -0700

    added parameters to tugboat provisioning script

--- digitalocean-newdroplet.sh
+++ digitalocean-newdroplet.sh
@@ -3,10 +3,17 @@
 # super easy digital ocean droplet provisioner
 # requires tugboat to be installed and configured
 
+# Parameters - edit these
+DROPLET_NAME_PREFIX="tempdroplet"
+DROPLET_REGION=3 # SFO1
+DROPLET_SIZE=66 # 512mb
+DROPLET_IMAGE=9801950 # ubuntu-14-04-x64
+DROPLET_SSH_KEY=317570 # must be the ssh_key id
+
 timestamp=$(date +"%H%M%S")
 droplet_name="temp-$timestamp"
 
-tugboat create -i 9801951 -k 585424 $droplet_name
-sleep 1
+tugboat create -i $DROPLET_IMAGE -k $DROPLET_SSH_KEY -s $DROPLET_SIZE -r $DROPLET_REGION $DROPLET_NAME_PREFIX
+sleep 5
 droplet_ip=$(tugboat droplets | grep -i $droplet_name | awk -F "[ ,]" '{print $3}')
 echo -e "\n$droplet_ip\n"
